de1bb1036eacb3ded3340f47866ad72de7a8d78a
Kanika <goyalkanu25@gmail.com>
2023-12-13 22:52:47 +0530

    day-6

---
+++ 1287. Element Appearing More Than 25% In Sorted Array.py
@@ -0,0 +1,12 @@
+from typing import List
+
+class Solution:
+    def findSpecialInteger(self, arr: List[int]) -> int:
+        n = len(arr)
+        quarter = n // 4
+
+        for i in range(n - quarter):
+            if arr[i] == arr[i + quarter]:
+                return arr[i]
+
+        raise ValueError("No special integer found")
